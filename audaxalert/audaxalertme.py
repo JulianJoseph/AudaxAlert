7599585722d1e0450f39ddc976beedea4c771606
Julian Joseph <julian@joseph-net.co.uk>
2019-02-18 14:43:25 +0000

    Update audaxalertme.py

--- audaxalert/audaxalertme.py
+++ audaxalert/audaxalertme.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SECRET_KEY'] = '\xba\x18<\xafuN9\xf9x\xaf1\xe0E\x88O\xf1Z{\xacN\xaf\xfa\xf6\xcc'
+app.config['SECRET_KEY'] = 'xxxxx'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'audaxalert.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 'false'
 app.config['DEBUG'] = True
