d387cf8c42290c19c09a6ffdf68e4f76253801df
Julian Joseph <julian@joseph-net.co.uk>
2019-02-18 14:40:49 +0000

    Update __init__.py

--- audaxalert/__init__.py
+++ audaxalert/__init__.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SECRET_KEY'] = '\xba\x18<\xafuN9\xf9x\xaf1\xe0E\x88O\xf1Z{\xacN\xaf\xfa\xf6\xcc' 
+app.config['SECRET_KEY'] = 'xxxx' 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'audaxalert.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 'false'
 app.config['DEBUG'] = True
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6LdFBzoUAAAAAO4mIRuyvATCSf9RkghyQzKY5LV5'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdFBzoUAAAAAF3sF68GirLn6UgQXoT3hd4qcs8O'
+app.config['RECAPTCHA_PUBLIC_KEY'] = 'xxxx'
+app.config['RECAPTCHA_PRIVATE_KEY'] = 'xxxx'
 
 
 
